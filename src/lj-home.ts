68e9bbca1dfa2ce97299b16dd13b019ae4b97984
James O'Donoghue <jodonogh1@gmail.com>
2021-05-27 11:30:01 -0700

    feat: add favicon and update site

--- src/lj-home.ts
+++ src/lj-home.ts
@@ -52,10 +52,26 @@ export class Home extends LitElement {
                 </div>
                 <div class="text-xl mb-4">
                     <a
-                        class="text-secondary-800 hover:text-secondary-600"
+                        class="text-secondary-800 hover:text-secondary-600 items-center inline-flex"
                         href="https://www.lhf.org/"
-                        >Living History Farms</a
                     >
+                        <span class="mr-2 h-6">Living History Farms</span>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 inline-flex"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                            />
+                        </svg>
+                    </a>
                 </div>
                 <div class="mb-4">
                     <div class="font-light">Ceremony</div>
@@ -76,10 +92,25 @@ export class Home extends LitElement {
                     <div class="flex items-baseline mb-4">
                         <div class="mr-8 text-xl">
                             <a
-                                class="text-xl text-secondary-800 hover:text-secondary-600"
+                                class="text-xl text-secondary-800 hover:text-secondary-600 inline-flex items-center"
                                 href="https://hotelrenovo.com/"
-                                >Hotel Renovo</a
-                            >
+                                ><span class="h-6 mr-2">Hotel Renovo</span>
+
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="h-5 w-5 inline-flex"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                                    />
+                                </svg>
+                            </a>
                         </div>
                         <div
                             class="bg-secondary-200 text-secondary-800 px-2 rounded-lg text-sm mr-4 font-body"
@@ -121,10 +152,54 @@ export class Home extends LitElement {
                 </div>
                 <div>
                     <a
-                        class="text-xl text-secondary-800 hover:text-secondary-600"
+                        class="text-xl text-secondary-800 hover:text-secondary-600 inline-flex items-center"
                         href="https://www.google.com/maps/dir/Des+Moines+International+Airport,+Fleur+Drive,+Des+Moines,+IA/Living+History+Farms,+11121+Hickman+Rd,+Urbandale,+IA+50322/@41.5652879,-93.7451459,13z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x87eea1f80909570b:0x80179b8d59f567a!2m2!1d-93.6587958!2d41.5341333!1m5!1m1!1s0x87ec20ccf91cbf7d:0x37ed847db7aac69a!2m2!1d-93.7705303!2d41.6169508!3e0"
-                        >DSM Airport to Living History Farms</a
-                    >
+                        ><span class="h-6 mr-2"
+                            >DSM Airport to Living History Farms</span
+                        >
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 inline-flex"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                            />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="mb-16">
+                <div class="text-3xl text-primary-800 mb-4">
+                    What am I wearing?
+                </div>
+                <div>
+                    <a
+                        class="text-xl text-secondary-800 hover:text-secondary-600 inline-flex items-center"
+                        href="https://www.brides.com/story/wedding-dress-code-explained#semi-formal-or-dressy-casual"
+                        ><span class="h-6 mr-2">Dressy casual</span>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 inline-flex"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                            />
+                        </svg>
+                    </a>
                 </div>
             </div>
         `;
